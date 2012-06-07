dd6a153f42588f10d6afb869a5487ac3f0b9b735
philananin <phil.ananin@gmail.com>
2012-06-07 16:55:31 +0100

    removed git config from install file

--- install.sh
+++ install.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# set git user
-[ -z `git config --global user.name` ] && git config --global user.name "philananin"
-[ -z `git config --global user.email` ] && git config --global user.email "phil.ananin@gmail.com"
-
-# coloured git output
-git config --global color.ui auto
-
 # symlinks
 [ ! -d ~/.vim ] && ln -s "$PWD/vim" ~/.vim
 [ ! -f ~/.vimrc ] && ln -s "$PWD/vimrc" ~/.vimrc
