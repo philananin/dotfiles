8170afe067a118f59488b35393a665a37e8f31d4
Phil Ananin <phil.ananin@gmail.com>
2014-05-06 22:29:28 +0100

    put the dot in dotfiles

--- install.sh
+++ install.sh
@@ -5,21 +5,26 @@ rm -rf ~/.vim
 rm ~/.vimrc
 rm ~/.zshrc
 rm ~/.tmux.conf
+rm ~/.tmuxline.conf
+rm ~/.gitconfig
+rm ~/.exports
+rm ~/.path
+rm ~/.aliases
 
 echo "install vundle"
 mkdir -p ~/.vim/bundle/vundle
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
 echo "create symlinks"
-ln -s $PWD/vimrc ~/.vimrc
+ln -s $PWD/.vimrc ~/.vimrc
 ln -s $PWD/bundles.vim ~/.vim/bundles.vim
-ln -s $PWD/zshrc ~/.zshrc
-ln -s $PWD/tmux.conf ~/.tmux.conf
-ln -s $PWD/tmuxline.conf ~/.tmuxline.conf
-ln -s $PWD/gitconfig ~/.gitconfig
-ln -s $PWD/exports ~/.exports
-ln -s $PWD/path ~/.path
-ln -s $PWD/aliases ~/.aliases
+ln -s $PWD/.zshrc ~/.zshrc
+ln -s $PWD/.tmux.conf ~/.tmux.conf
+ln -s $PWD/.tmuxline.conf ~/.tmuxline.conf
+ln -s $PWD/.gitconfig ~/.gitconfig
+ln -s $PWD/.exports ~/.exports
+ln -s $PWD/.path ~/.path
+ln -s $PWD/.aliases ~/.aliases
 
 echo "installing vundle packages"
 vim -u ~/.vim/bundles.vim +BundleInstall +qall
